2c092c0caaa0ebda9f131df04b429c0899bfd61d
Daniel Perez <danielperezvl@gmail.com>
2019-01-19 18:46:12 +0000

    Add dot rendering, mean average calculation

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const outputY = document.getElementById('outputY');
 const matrixOffset = matrix.getClientRects()[0];
 const scaleXMax = matrixOffset.width;
 const scaleYMax = matrixOffset.height;
+const readings = [];
 
 matrix.addEventListener('click', (event) => {
   const percX = percentalize(event.clientX - matrixOffset.left, scaleXMax);
@@ -12,13 +13,36 @@ matrix.addEventListener('click', (event) => {
   outputX.innerHTML = percX;
   outputY.innerHTML = percY;
 
-  const clickPointEl = document.querySelector('.click-point').cloneNode(true);
-  clickPointEl.style.position = 'absolute';
-  clickPointEl.style.left = `${percX}%`;
-  clickPointEl.style.top = `${percY}%`;
-  matrix.appendChild(clickPointEl);
+  readings.push([percX, percY]);
+
+  renderClick(percX, percY, matrix);
 });
+
+function renderClick (x, y, context, className) {
+  const clickDot = document.createElement('div');
+  clickDot.className = `click-point ${className}`;
+  clickDot.style.position = 'absolute';
+  clickDot.style.left = `${x}%`;
+  clickDot.style.top = `${y}%`;
+  context.appendChild(clickDot);
+}
 
 function percentalize (value, max) {
   return value * 100 / max;
+}
+
+// Calculates two dimentional array mean for each row
+function renderMean (values) {
+  let meanX = 0;
+  let meanY = 0;
+
+  values.forEach(reading => {
+    meanX += reading[0];
+    meanY += reading[1];
+  });
+
+  meanX = meanX / values.length;
+  meanY = meanY / values.length;
+
+  renderClick(meanX, meanY, matrix, 'click-point--big');
 }
