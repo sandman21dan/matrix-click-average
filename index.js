2c793656c92b54e83b55721341f70823745e3046
Daniel Perez <danielperezvl@gmail.com>
2019-01-19 14:43:40 +0000

    Convert to percentage, render click point

--- index.js
+++ index.js
@@ -6,12 +6,19 @@ const scaleXMax = matrixOffset.width;
 const scaleYMax = matrixOffset.height;
 
 matrix.addEventListener('click', (event) => {
-  outputX.innerHTML = percentalize(event.clientX - matrixOffset.left, scaleXMax);
-  outputY.innerHTML = percentalize(event.clientY - matrixOffset.top, scaleYMax);
+  const percX = percentalize(event.clientX - matrixOffset.left, scaleXMax);
+  const percY = percentalize(event.clientY - matrixOffset.top, scaleYMax);
+
+  outputX.innerHTML = percX;
+  outputY.innerHTML = percY;
+
+  const clickPointEl = document.querySelector('.click-point').cloneNode(true);
+  clickPointEl.style.position = 'absolute';
+  clickPointEl.style.left = `${percX}%`;
+  clickPointEl.style.top = `${percY}%`;
+  matrix.appendChild(clickPointEl);
 });
 
 function percentalize (value, max) {
   return value * 100 / max;
 }
-
-
